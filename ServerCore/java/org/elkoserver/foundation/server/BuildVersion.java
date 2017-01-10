b51c2a16e5c167dc3832c7bd925baa0a7015b6fd
Steve Salevan <steve.salevan@gmail.com>
2017-01-10 13:03:34 -0800

    Adds Maven buildfile as well as Mongo API changes

---
+++ ServerCore/java/org/elkoserver/foundation/server/BuildVersion.java
@@ -0,0 +1,15 @@
+package org.elkoserver.foundation.server;
+
+/* Note that this source file is a template.  The source that will actually be
+   compiled for the BuildVersion class is generated from this template at build
+   time by the Makefile. */
+
+/**
+ * Struct class, whose sole purpose is to hold onto a statically configured
+ * version identification string.
+ */
+class BuildVersion {
+    /** The version of Elko that this build represents. */
+    static public final String version = "Elko 2.0.1 Release build 10-Jan-2017 12:42:19 PST";
+}
+
